b9afc063d41dd36d65aad15124fb748c0d118d47
Jeffe <61993619+jenumerables@users.noreply.github.com>
2023-01-25 22:20:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import jsonify
+
+def get_currency(in_currency, out_currency):
+  url = f'https://www.x-rates.com/calculator/?from={in_currency}&to={out_currency}&amount=1'
+  content = requests.get(url).text
+  soup = BeautifulSoup(content, 'html.parser')
+  rate = soup.find("span", class_="ccOutputRslt").get_text()
+  rate = float(rate[:-4])
+
+  return rate
+
+
+# __name__ special variable that holds the main.py script
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return '<h1>Currency Rate API</h1><p>Example URL: /api/v1/usd-eur<p>'
+
+@app.route('/api/v1/<in_cur>-<out_cur>')
+def api(in_cur, out_cur):
+  rate = get_currency(in_cur, out_cur)
+  result_dictionary = {'input_currency': in_cur, 'output_currency':out_cur,'rate':rate}
+  return jsonify(result_dictionary)
+
+  
+app.run(host='0.0.0.0')
